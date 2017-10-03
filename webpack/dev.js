d9fc249a88637bf88aa8785fd217b988b2f35cd4
Chyzwar <schizek.marcin@gmail.com>
2017-10-03 22:42:19 +0100

    eslint in webpack

--- webpack/dev.js
+++ webpack/dev.js
@@ -9,44 +9,42 @@ const commonConfig = require('./common');
  *
  * @see
  */
-module.exports = (env) => {
-  return webpackMerge(commonConfig, {
-    devtool: 'inline-source-map',
-    cache: true,
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'inline-source-map',
+  cache: true,
     /**
      * Add aditional entry for debugging
      */
-    entry: {
+  entry: {
 
-    },
+  },
     /**
      * DevServer Configuration
      *
      * @see https://webpack.js.org/configuration/dev-server/#devserver
      */
-    devServer: {
-      contentBase: path.resolve('build'),
-      compress: true,
-      port: 3000,
-      hot: true,
-      inline: true,
-    },
-    plugins: [
-      /**
-       * Plugin: HotModuleReplacementPlugin
-       *
-       * @see https://webpack.js.org/plugins/hot-module-replacement-plugin/
-       */
-      new webpack.HotModuleReplacementPlugin(),
-      /**
-       * Plugin: DefinePlugin, strigify in source code
-       *
-       * NOTE: when adding more properties make sure you include them in src/typings.d.ts
-       * @see https://webpack.js.org/plugins/define-plugin/
-       */
-      new webpack.DefinePlugin({
-        NODE_ENV: JSON.stringify('development'),
-      }),
-    ],
-  });
-};
+  devServer: {
+    contentBase: path.resolve('build'),
+    compress: true,
+    port: 3000,
+    hot: true,
+    inline: true,
+  },
+  plugins: [
+    /**
+     * Plugin: HotModuleReplacementPlugin
+     *
+     * @see https://webpack.js.org/plugins/hot-module-replacement-plugin/
+     */
+    new webpack.HotModuleReplacementPlugin(),
+    /**
+     * Plugin: DefinePlugin, strigify in source code
+     *
+     * NOTE: when adding more properties make sure you include them in src/typings.d.ts
+     * @see https://webpack.js.org/plugins/define-plugin/
+     */
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify('development'),
+    }),
+  ],
+});
